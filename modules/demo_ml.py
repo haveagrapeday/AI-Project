12dc106b7ebab1c97c45a6bdd195fee63f5543a8
haveagrapeday <uoobuv128@gmail.com>
2025-03-16 06:45:15 +0700

    Update demo_ml.py

--- modules/demo_ml.py
+++ modules/demo_ml.py
@@ -70,38 +70,4 @@ def show():
         
        
 
-        # 🔹 9. Character Personality Traits Demo
-        st.subheader("🎭 Character Personality Traits")
-        characters = {
-            "Harry Potter": ([8, 7, 6, 5, 2, 9, 6, 4], "Gryffindor"),
-            "Hermione Granger": ([5, 10, 5, 4, 1, 2, 5, 8], "Gryffindor"),
-            "Ron Weasley": ([6, 3, 8, 3, 2, 6, 4, 5], "Gryffindor"),
-            "Draco Malfoy": ([3, 4, 2, 9, 7, 3, 5, 2], "Slytherin"),
-            "Albus Dumbledore": ([9, 10, 8, 6, 5, 2, 10, 10], "Gryffindor"),
-            "Rubeus Hagrid": ([5, 6, 6, 1, 4, 0, 5, 8], "Gryffindor"),
-            "Severus Snape": ([6, 7, 10, 4, 8, 1, 7, 6], "Slytherin"),
-            "Voldemort": ([3, 5, 1, 10, 10, 1, 8, 4], "Slytherin"),
-            "Minerva McGonagall": ([8, 10, 9, 2, 2, 1, 5, 7], "Slytherin"),
-            "Luna Lovegood": ([6, 9, 8, 2, 1, 1, 2, 10], "Ravenclaw"),
-            "Gilderoy Lockhart": ([2, 3, 1, 5, 3, 1, 3, 6], "Ravenclaw"),
-            "Cedric Diggory": ([5, 2, 3, 7, 1, 8, 6, 3], "Hufflepuff"),
-        }
-        
-        character_names = list(characters.keys())
-        selected_character = st.selectbox("Select a Main Character", character_names)
-        selected_traits, house = characters[selected_character]
-        
-        trait_colors = ["#B22222", "#4682B4", "#FFD700", "#2E8B57", "#2E8B57", "#B22222", "#B22222", "#FFD700"]
-        
-        traits = ["Bravery", "Intelligence", "Loyalty", "Ambition", "Dark Arts Knowledge", "Quidditch Skills", "Dueling Skills", "Creativity"]
-        
-        fig, ax = plt.subplots()
-        sns.barplot(x=selected_traits, y=traits, palette=trait_colors, ax=ax)
-        ax.set_xlabel("Trait Score")
-        ax.set_ylabel("Trait")
-        ax.set_title(f"Personality Traits of {selected_character} ({house})")
-        st.pyplot(fig)
-        
-    else:
-        st.warning("❌ Data files not found! Please ensure that the files 'harry_potter_1000_students.csv' and 'Dialogue.csv' are in the correct directory.")
-
+       
